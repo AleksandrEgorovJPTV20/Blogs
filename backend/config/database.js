dc2e82019000365e147c2b46faf1aad3767795bb
pupil <pupil@ivkhk.ee>
2023-10-13 14:04:34 +0300

    Создал backup. Сохраняем все коллекции в backup файле при запуске проекта

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,6 +1,7 @@
-//Mongoose - это библиотека для Node.js, которая предоставляет средства для работы с MongoDB, одной из популярных NoSQL баз данных.
 require('dotenv').config();
 const mongoose = require('mongoose');
+const path = require('path');
+const fs = require('fs');
 
 const connectDB = async () => {
     try {
@@ -8,10 +9,33 @@ const connectDB = async () => {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
+
         console.log('Connected to MongoDB');
+
+        // Create a backup after connecting to the database
+        createBackup();
+
     } catch (err) {
         console.error('Error connecting to MongoDB:', err);
     }
-}
+};
+
+
+const createBackup = async () => {
+    const collections = await mongoose.connection.db.collections(); //Получаем все коллекции из базы данных
+    const backupData = {};
+
+    //Перебираем каждую коллекцию
+    for (const collection of collections) {
+        const name = collection.collectionName; //Для каждой коллекции извлекается ее имя. Это имя используется в качестве ключа в объекте backupData для организации данных.
+        const data = await collection.find({}).toArray(); //Извлекаются все записи из текущей коллекции. Преобразуем результат запроса в массив объектов
+        backupData[name] = data;
+    }
+
+    //Заполняем данные файл в JSON формате
+    const backupFilePath = path.join(__dirname, 'backup.json');
+    fs.writeFileSync(backupFilePath, JSON.stringify(backupData, null, 2)); //Используется для преобразования объекта JavaScript в строку JSON. Аргументы null и 2 используются для форматирования JSON с отступами.
+    console.log('Backup created at', backupFilePath);
+};
 
 module.exports = connectDB;
